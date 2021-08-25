f1304ebd80d5697e922927788c525df4e866937b
Vien V.Nguyen <nguyenvanvienqn@gmail.com>
2021-08-25 15:19:51 +0700

    Add gradient bars for BarChart

--- Source/Charts/Utils/Platform.swift
+++ Source/Charts/Utils/Platform.swift
@@ -152,6 +152,27 @@ types are aliased to either their UI* implementation (on iOS) or their NS* imple
 
     extension NSUIColor
     {
+        var nsuirgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)?
+        {
+            var red: CGFloat = 0
+            var green: CGFloat = 0
+            var blue: CGFloat = 0
+            var alpha: CGFloat = 0
+
+            guard let colorSpaceModel = cgColor.colorSpace?.model else {
+                return nil
+            }
+            guard colorSpaceModel == .rgb else {
+                return nil
+            }
+
+            getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+            return (red: red, green: green, blue: blue, alpha: alpha)
+        }
+    }
+
+    extension NSUIColor
+    {
         var nsuirgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)? {
             var red: CGFloat = 0
             var green: CGFloat = 0
